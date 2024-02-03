ae5c405ce7109e1017a0bd804e565a70470ca8d8
mtnmunuklu <mtnmunuklu@gmail.com>
2024-02-03 17:55:27 +0300

    Add unit test for FetchNVDFeeds handler
    This commit introduces a new unit test for the FetchNVDFeeds handler in the VulnHandlers. The test covers the interaction with Auth Service and Vuln Service, ensuring that GetUserRole and FetchNVDFeeds functions are called with the expected parameters. The simulated data returned by the mockVulnWrapper.StreamClientWrapper is used to validate the behavior of the FetchNVDFeeds handler. This test aims to enhance the test coverage and ensure the correctness of the FetchNVDFeeds endpoint.

--- api/handlers/vuln_test.go
+++ api/handlers/vuln_test.go
@@ -185,13 +185,13 @@ func (c *MockVulnServiceClientWrapper) FetchNVDFeeds(ctx context.Context, req *p
 	return nil, nil
 }
 
-type MockVulnService_GetAllCVEsClientWrapper struct {
+type MockVulnService_StreamClientWrapper struct {
 	cves []*pb.CVE
 	idx  int
 }
 
 // Recv simulates receiving CVEs from the server.
-func (m *MockVulnService_GetAllCVEsClientWrapper) Recv() (*pb.CVE, error) {
+func (m *MockVulnService_StreamClientWrapper) Recv() (*pb.CVE, error) {
 	if m.idx < len(m.cves) {
 		cve := m.cves[m.idx]
 		m.idx++
@@ -201,47 +201,47 @@ func (m *MockVulnService_GetAllCVEsClientWrapper) Recv() (*pb.CVE, error) {
 }
 
 // CloseSend simulates closing the send stream.
-func (m *MockVulnService_GetAllCVEsClientWrapper) CloseSend() error {
+func (m *MockVulnService_StreamClientWrapper) CloseSend() error {
 	return nil
 }
 
 // Context returns the client context.
-func (m *MockVulnService_GetAllCVEsClientWrapper) Context() context.Context {
+func (m *MockVulnService_StreamClientWrapper) Context() context.Context {
 	return context.Background()
 }
 
 // SendMsg simulates sending a message.
-func (m *MockVulnService_GetAllCVEsClientWrapper) SendMsg(m1 interface{}) error {
+func (m *MockVulnService_StreamClientWrapper) SendMsg(m1 interface{}) error {
 	return nil
 }
 
 // RecvMsg simulates receiving a message.
-func (m *MockVulnService_GetAllCVEsClientWrapper) RecvMsg(m1 interface{}) error {
+func (m *MockVulnService_StreamClientWrapper) RecvMsg(m1 interface{}) error {
 	return nil
 }
 
 // Header returns the header metadata.
-func (m *MockVulnService_GetAllCVEsClientWrapper) Header() (metadata.MD, error) {
+func (m *MockVulnService_StreamClientWrapper) Header() (metadata.MD, error) {
 	return nil, nil
 }
 
 // Trailer returns the trailer metadata.
-func (m *MockVulnService_GetAllCVEsClientWrapper) Trailer() metadata.MD {
+func (m *MockVulnService_StreamClientWrapper) Trailer() metadata.MD {
 	return nil
 }
 
 // SendHeader simulates sending header metadata.
-func (m *MockVulnService_GetAllCVEsClientWrapper) SendHeader(m1 metadata.MD) error {
+func (m *MockVulnService_StreamClientWrapper) SendHeader(m1 metadata.MD) error {
 	return nil
 }
 
 // SetSendDeadline simulates setting the send deadline.
-func (m *MockVulnService_GetAllCVEsClientWrapper) SetSendDeadline(m1 time.Time) error {
+func (m *MockVulnService_StreamClientWrapper) SetSendDeadline(m1 time.Time) error {
 	return nil
 }
 
 // SetRecvDeadline simulates setting the receive deadline.
-func (m *MockVulnService_GetAllCVEsClientWrapper) SetRecvDeadline(m1 time.Time) error {
+func (m *MockVulnService_StreamClientWrapper) SetRecvDeadline(m1 time.Time) error {
 	return nil
 }
 
@@ -331,7 +331,7 @@ func TestGetAllCVEs(t *testing.T) {
 			{Id: "2", CveId: "CVE-2022-5678", Description: "Test CVE 2", Severity: "Medium", Product: "Test Product 2", Vendor: "Test Vendor 2", Published: "2024-01-27T10:20:20", Modified: "2024-01-27T10:20:20"},
 		}
 
-		mockStream := &MockVulnService_GetAllCVEsClientWrapper{
+		mockStream := &MockVulnService_StreamClientWrapper{
 			cves: cves,
 		}
 
@@ -462,4 +462,60 @@ func TestUpdateCVE(t *testing.T) {
 
 	// Release the Fiber context
 	app.ReleaseCtx(fiberContext)
+}
+
+func TestFetchNVDFeeds(t *testing.T) {
+
+	// Create a custom mock client wrapper for Auth Service
+	mockAuthWrapper := &MockAuthServiceClientWrapper{}
+
+	// Create a custom mock client wrapper for Vuln Service
+	mockVulnWrapper := &MockVulnServiceClientWrapper{}
+
+	// Create handlers using the custom mock client wrapper
+	handler := NewVulnHandlers(mockAuthWrapper, mockVulnWrapper)
+
+	// Set Auth Service Client in the mockWrapper
+	mockAuthWrapper.GetUserRoleFunc = func(ctx context.Context, req *pb.GetUserRoleRequest, opts ...grpc.CallOption) (*pb.GetUserRoleResponse, error) {
+		// Simulate the behavior of the gRPC service
+		return &pb.GetUserRoleResponse{Role: "admin"}, nil
+	}
+
+	// Set Vuln Service Client in the mockWrapper
+	mockVulnWrapper.FetchNVDFeedsFunc = func(ctx context.Context, req *pb.FetchNVDFeedsRequest, opts ...grpc.CallOption) (pb.VulnService_FetchNVDFeedsClient, error) {
+		// Simulate the behavior of the gRPC service
+		cves := []*pb.CVE{
+			{Id: "1", CveId: "CVE-2022-1234", Description: "Test CVE 1", Severity: "High", Product: "Test Product 1", Vendor: "Test Vendor 1", Published: "2024-01-27T10:10:10", Modified: "2024-01-27T10:10:10"},
+			{Id: "2", CveId: "CVE-2022-5678", Description: "Test CVE 2", Severity: "Medium", Product: "Test Product 2", Vendor: "Test Vendor 2", Published: "2024-01-27T10:20:20", Modified: "2024-01-27T10:20:20"},
+		}
+
+		mockStream := &MockVulnService_StreamClientWrapper{
+			cves: cves,
+		}
+
+		return mockStream, nil
+	}
+
+	// Create a Fiber context
+	app := fiber.New()
+	fiberContext := app.AcquireCtx(&fasthttp.RequestCtx{})
+
+	// Set the request headers in the Fiber context
+	userId := bson.NewObjectId()
+	token, err := security.NewToken(userId.Hex())
+	assert.NoError(t, err)
+	fiberContext.Request().Header.Set("Authorization", "Bearer "+token+"")
+	fiberContext.Request().Header.Set("CveId", "123")
+
+	// Test the DeleteCVE handler
+	err = handler.FetchNVDFeeds(fiberContext)
+	assert.NoError(t, err)
+
+	// Assert that the GetUserRole and FetchNVDFeeds functions were called with the expected parameters
+	assert.True(t, mockAuthWrapper.GetUserRoleFuncCalled, "GetUserRole function of mockWrapper should be called")
+	assert.True(t, mockVulnWrapper.FetchNVDFeedsFuncCalled, "FetchNVDFeeds function of mockWrapper should be called")
+
+	// Release the Fiber context
+	app.ReleaseCtx(fiberContext)
+
 }
