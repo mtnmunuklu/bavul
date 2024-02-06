f50789c4ef539b552eb14df0ea06f938359456de
mtnmunuklu <mtnmunuklu@gmail.com>
2024-02-06 21:05:05 +0300

    Update TestValidateToken for token expiration
    Update the TestValidateToken function to include testing token expiration. The test now includes waiting for the token to expire, generating an expired token, and ensuring that validation returns an error with the appropriate message.

--- security/token_test.go
+++ security/token_test.go
@@ -19,20 +19,32 @@ func TestNewToken(t *testing.T) {
 
 // TestValidateToken tests the validation of a token.
 func TestValidateToken(t *testing.T) {
+	// Generate a new ObjectId for testing purposes
 	id := bson.NewObjectId()
+
+	// Create a new token with the generated ObjectId
 	token, err := NewToken(id.Hex())
 	assert.NoError(t, err)
 	assert.NotEmpty(t, token)
 
+	// Validate the token and ensure no errors
 	userId, err := ValidateToken(token)
 	assert.NoError(t, err)
 	assert.NotNil(t, userId)
 	assert.Equal(t, id.Hex(), userId)
 
+	// Wait for the token to expire (e.g., 1 second) to test expiration
+	time.Sleep(time.Second)
+
+	// Generate an expired token for testing purposes
 	tokenExpired := GetTokenExpired(id.Hex())
+
+	// Attempt to validate the expired token and ensure it returns an error
 	userId, err = ValidateToken(tokenExpired)
 	assert.Error(t, err)
-	assert.Nil(t, userId)
+	assert.Equal(t, userId, "")
+	assert.EqualError(t, err, "token has invalid claims: token is expired")
+
 }
 
 // GetTokenExpired tests the operation of receiving the token expire duration.
